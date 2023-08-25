6caf60284b19875cafa72d6f2356185e3358d018
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-08-25 10:25:49 +0200

    style navbar

--- src/main/scala/testvite/Navbar.scala
+++ src/main/scala/testvite/Navbar.scala
@@ -7,16 +7,14 @@ final case class NavBar() extends Component {
 
   def body: Div =
     div(
-      justifyContent.spaceBetween,
-      div(
-        textTransform.uppercase,
-        display.flex,
-        flexDirection.row,
-        fontSize("18px"),
-        navLink("Home", HomePage),
-        navLink("Object", ViewObject(1)),
-        navLink("Search", Search("aa")),
-      )
+      textTransform.uppercase,
+      display.flex,
+      flexDirection.row,
+      justifyContent.spaceEvenly,
+      fontSize("18px"),
+      navLink("Home", HomePage),
+      navLink("Object", ViewObject(1)),
+      navLink("Search", Search("aa")),
     )
 
   private def navLink(
