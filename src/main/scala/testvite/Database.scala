c6bae34e8de4edff96c74449d3d322b08243b1f7
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-08-26 14:53:53 +0200

    refactor database

--- src/main/scala/testvite/Database.scala
+++ src/main/scala/testvite/Database.scala
@@ -1,29 +1,35 @@
 package testvite
 
 val entities = Map(
-  1 -> Entity(
-    1,
-    "one",
-    Set(Relation(1, 2), Relation(1, 3)),
-    Set()
-  ),
-  2 -> Entity(
-    2,
-    "two",
-    Set(Relation(2, 3)),
-    Set(),
-  ),
-  3 -> Entity(
-    3,
-    "three",
-    Set(Relation(3, 1)),
-    Set()
-  ),
+  1 -> "one",
+  2 -> "two",
+  3 -> "three",
+  4 -> "four",
 )
 
-case class Database(private val entities: Map[Id, Entity]):
-  def search(query: String): List[Entity] = entities.values.toList.filter(_.value.contains(query))
-  def get(id: Id): Option[Entity] = entities.get(id)
+val relations = Set(
+  Relation(1, 2),
+  Relation(1, 3),
+  Relation(2, 3),
+  Relation(3, 1),
+)
+
+case class Database(private val entities: Map[Id, Value], private val relations: Set[Relation]):
+  def search(query: String): List[Entity] =
+    entities
+      .filter((id, value) => value.contains(query))
+      .map((id, value) => Entity(
+        id,
+        value,
+        relations.filter(r => r.from == id).toSet,
+        relations.filter(r => r.to == id).toSet,
+      )).toList
+  def get(id: Id): Option[Entity] = entities.get(id).map(value => Entity(
+        id,
+        value,
+        relations.filter(r => r.from == id).toSet,
+        relations.filter(r => r.to == id).toSet,
+      ))
 
 object Database:
-  val dummy = Database(entities)
+  val dummy = Database(entities, relations)
