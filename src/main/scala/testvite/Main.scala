be69c4c1109f77bb4d6deaccdf28dfcca958968c
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-08-26 13:13:12 +0200

    fix css loading

--- src/main/scala/testvite/Main.scala
+++ src/main/scala/testvite/Main.scala
@@ -2,10 +2,13 @@ package testvite
 
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters.*
+import scala.scalajs.js.`import`
+import scala.scalajs.js.annotation.JSImport
 
 import com.raquo.laminar.api.L.{*, given}
 
 import org.scalajs.dom
+import scalacss.DevDefaults._
 
 object Main {
   final class DataItemID
@@ -21,8 +24,15 @@ object Main {
   val allValues = dataSignal.map(_.map(_.value))
 
   def main(args: Array[String]): Unit = {
-    // Laminar initialization
-    renderOnDomContentLoaded(dom.document.querySelector("#app"), appElement())
+    windowEvents(_.onLoad).foreach { _ =>
+      val sty = styleTag(CSS.render[String])
+      dom.document.querySelector("head").appendChild(sty.ref)
+      val containerNode = dom.document.querySelector("#app")
+      render(
+        containerNode,
+        appElement(),
+      )
+    }(unsafeWindowOwner)
   }
 
   def appElement(): HtmlElement = {
