30408ef73e3dad5ab4acd8eea5b5cc5e9870f93d
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-09-09 14:02:40 +0200

    data for reconciliation example

--- src/main/scala/testvite/Data.scala
+++ src/main/scala/testvite/Data.scala
@@ -4,7 +4,12 @@ val data =
   """
   {
     "values": {
-      "v-1": "hm"
+      "v-1": "Arrival",
+      "v-2": "Gravity",
+      "v-3": "Amy Adams",
+      "v-4": "Louise Banks",
+      "v-5": "Jeremy Renner",
+      "v-6": "Ian Donnelly"
     },
     "relations": []
   }
