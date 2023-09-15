9c885d55fedd052f12c2b4c4f8f950be4961cbfa
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-09-15 14:31:06 +0200

    remove style css

--- src/main/scala/testvite/CSS.scala
+++ src/main/scala/testvite/CSS.scala
@@ -5,6 +5,21 @@ import scalacss.DevDefaults._
 object CSS extends StyleSheet.Inline {
   import dsl._
 
+  val ff = fontFace("appFont")(_
+    .src("local(Avenir)")
+    .fontStretch.ultraCondensed
+    .fontWeight._200)
+
+
+  val app = style(
+    fontFamily(ff),
+    //`-webkit-font-smoothing`: antialiased,
+    //`-moz-osx-font-smoothing`: grayscale,
+    textAlign.center,
+    color(c"rgb(245, 135, 31)"),
+    marginTop(60.px),
+  )
+
   val yellowBack = style(
     backgroundColor.yellow,
     fontWeight.bold,
--- src/main/scala/testvite/Main.scala
+++ src/main/scala/testvite/Main.scala
@@ -8,6 +8,7 @@ import scalacss.DevDefaults._
 object Main {
   def main(args: Array[String]): Unit = {
     windowEvents(_.onLoad).foreach { _ =>
+      println(CSS.render)
       val sty = styleTag(CSS.render[String])
       dom.document.querySelector("head").appendChild(sty.ref)
       val containerNode = dom.document.querySelector("#app")
