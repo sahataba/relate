76255bd5a0ecc73bf8efce406ac28a33f6c27e9d
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-10-14 19:40:35 +0200

    scalacss remove unused

--- src/main/scala/testvite/CSS.scala
+++ src/main/scala/testvite/CSS.scala
@@ -4,30 +4,6 @@ import scalacss.DevDefaults._
 
 object CSS extends StyleSheet.Inline {
   import dsl._
-
-  object colors:
-    val textColor = c"rgb(0, 0, 0)"
-
-  val ff = fontFace("appFont")(
-    _.src("local(Avenir)").fontStretch.ultraCondensed.fontWeight._200
-  )
-
-  val app = style(
-    fontFamily(ff),
-    // `-webkit-font-smoothing`: antialiased,
-    // `-moz-osx-font-smoothing`: grayscale,
-    textAlign.center,
-    color(colors.textColor),
-    marginTop(60.px)
-  )
-
-  val yellowBack = style(
-    backgroundColor.yellow,
-    fontWeight.bold,
-    &.hover -
-      cursor.zoomIn
-  )
-
   val navLink = styleF.bool(ok =>
     styleS(
       if (ok) textDecorationLine.underline else textDecorationLine.none,
