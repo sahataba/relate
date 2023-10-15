3da4d8a6b91f16b12787a8315c7be5832c69837d
Rudolf Markulin <rudolf.markulin@protonmail.com>
2023-10-15 14:37:12 +0200

    underline inner sentence

--- src/main/scala/testvite/Components.scala
+++ src/main/scala/testvite/Components.scala
@@ -115,6 +115,7 @@ def selectRelation(relation: Relation, selectedRelationComp: Option[Var[Option[S
 def relationSentence(relation: Relation, dbVar: Var[Database], selectedRelationComp: Option[Var[Option[SelectedRelation]]], viewKind: ViewKind, level: Int = 0): HtmlElement = {
   div(
     simpleInline,
+    if (level == 1) borderBottom("1px solid #aaa") else Modifier.empty,
     if (viewKind == "relation") div() else viewId(relation.subject, dbVar, selectedRelationComp, level),
     if(relation.predicate != Predicate.blank) selectRelation(relation, selectedRelationComp, "ExtractObjectSetPredicate") else div(),
     viewId(relation.predicate, dbVar, selectedRelationComp, level),
